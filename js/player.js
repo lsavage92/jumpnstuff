357b1cecb6e1fc28e5c5e3fe3dfc80b94261aafc
Colton Savage <csavage1994@gmail.com>
2015-11-19 20:54:19 -0600

    Changed jump method to smoothly move player up

--- js/player.js
+++ js/player.js
@@ -3,7 +3,7 @@ var Player = function(){
     $('body').keyup(function(e){
       if(e.keyCode === 32){
         $('.player').animate({
-          fill: "black"
+           y: '20%'
         }, 1000, function(){
           console.log('animation done');
         })
